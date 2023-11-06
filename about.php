dd0865331e337a2b27e9642313846eb141143642
kolen1231 <150078762+kolen1231@users.noreply.github.com>
2023-11-06 21:23:40 +0100

    Initial commit

---
+++ about.php
@@ -0,0 +1,348 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>TAGS - Tags</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About"/>
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering."/>
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png"/>
+<meta property="og:image:width" content="1200"/>
+<meta property="og:image:height" content="630"/>
+<meta property="og:url" content="https://rpcs3.net"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="RPCS3"/>
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Tags</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Basic information about tags in hmtl
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>BASICS ABOUT TAG 'a'</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The 'a' tag is one of the essential HTML elements used for creating links. Depending on the presence of the name or href attributes, the 'a' tag establishes either a link or an anchor. An anchor refers to a bookmark within a page, which can be specified as the target of a link. When using a link that points to an anchor, it leads to that bookmark within the web page. <br>
+									<br>
+									To create a link, you need to inform the browser that it's a link and provide the address of the document to which the link should navigate. The href attribute's value represents the document's address (URL, Universal Resource Locator) to which the navigation occurs. The link address can be either absolute or relative. Absolute addresses work universally, regardless of the website's name or the web page where the link is specified.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://th.bing.com/th/id/R.ee1a39fc303fbc7896c93de972e8935d?rik=AM9XiJZvhMZFKg&pid=ImgRaw&r=0') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('https://th.bing.com/th/id/OIP.zeQQI01dX5phMHsK24n0EwHaE8?pid=ImgDet&rs=1') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'acronym' tag</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									The 'acronym' tag is used to indicate that the enclosed text is an acronym. Unlike an abbreviation, an acronym is a well-established shortening that is used as an independent word. Some examples of acronyms include AIDS, DOS, USA, FAQ, and more. <br>
+									<br>
+									 By default, text enclosed in the 'acronym' container is underlined with a dotted line.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'abbr' tag</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The 'abbr' tag indicates that a sequence of characters is an abbreviation. The title attribute is used to provide the expansion or full meaning of the abbreviation. This helps people who may not be familiar with the abbreviation to understand its meaning. Additionally, search engines index the full-text version of the abbreviation, which can be used to improve a document's ranking.<br>
+									<br>
+									 Browsers do not typically style the text inside the 'abbr' tag, except for Opera versions prior to 15, which add a dotted underline to the text.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://th.bing.com/th/id/OIP.CU_kE48xjppPl-XsTFi_bQHaEN?pid=ImgDet&rs=1') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'address' tag </h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 
+									The 'address' tag is intended for storing information about the author and can include various HTML elements such as links, text, formatting, and more. It is expected that search engines will analyze the content within this tag to gather information about the authors of websites. <br>
+									<br>
+									By default, text within the 'address' container is displayed in italics. If you don't want this italic styling, you can use CSS styles to change the font style.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://th.bing.com/th/id/OIP.4iPqCHR8zh_CxL8ywOzydwHaE8?pid=ImgDet&rs=1') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'applet' tag </h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									The 'applet' tag is used for embedding applets, which are small programs written in the Java programming language, into a web page. This tag is considered obsolete, and it is recommended to use the more flexible 'object' tag instead. You can include text between the opening and closing tags, which will be displayed in the browser if it does not support applets. Otherwise, the text is not displayed. <br>
+									<br>
+									 
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://th.bing.com/th/id/R.f6f4f23d745cb0649c74ff66d5e0e260?rik=s3fwFQNE1NiEcw&pid=ImgRaw&r=0') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'area' element</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Each 'area' element defines active image areas that act as links. An image with these active areas is collectively called an image map. To the viewer, an image map appears no different from a regular image, but it can be divided into invisible areas of various shapes, with each area serving as a link. The 'area' tag specifies the shape and dimensions of the area, sets the URL of the linked document, and defines the name of the window or frame in which the document should be loaded by the browser. This tag is always placed within a <map> container, which associates the coordinates of the areas with the image.<br>
+									<br>
+									 Multiple areas can overlap, and the one that appears higher in the HTML code will be on top. 
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://cdn.wallpapersafari.com/81/64/Y3Z75L.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'article' tag</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The 'article' tag defines the content of a website, such as news, articles, blog posts, forum entries, and more.<br>
+									<br>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://1.bp.blogspot.com/-yfnIkeXJlGc/XqadhzfV67I/AAAAAAAAAPM/DHEQb47s8jUPFDKCnLGfhVsLUdYGswJ6QCLcBGAsYHQ/s1600/WHAT%2BIS%2BPROGRAMMING%2B1.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'basefont' tag</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>	
+									The 'basefont' tag is used to specify the default font, size, and color for text within an HTML document. The values specified will be applied to the entire document, except within the 'font' tag, where you can override the text styling. The 'basefont' tag can be used within the 'head' or 'body' section of the document, and it can be used multiple times to change the font style for different parts of the document. <br>
+									<br>
+				
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://th.bing.com/th/id/OIP.RKLhgVgzLfiyHIAwVLhkhAHaEK?pid=ImgDet&rs=1') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'bdo' tag </h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The 'bdo' tag is used to set the direction for text rendering, primarily intended for languages where reading occurs from right to left. For example, it is often used for languages like Arabic. <br>
+									<br>
+									
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://th.bing.com/th/id/R.26625f9c03dda4f04ca018b09d860f27?rik=PrEamteFcecw3Q&pid=ImgRaw&r=0') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'bgsound' tag</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The 'bgsound' tag defines a musical audio file that will play on a web page when it is opened. Attributes of the tag, such as volume, duration of the sound, and other characteristics, can be specified, and they can also be controlled through scripts. This tag should only be placed within the 'head' container. <br>
+									<br>
+									 It's important to note that the 'bgsound' tag is not part of the official HTML specification, and using it may result in code that doesn't pass validation. Additionally, users often have a negative perception of automatically playing music on websites, so it should be used with caution and only when necessary.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://www.e-kern.com/fileadmin/user_upload/Images_Allgemein/Kacheln/Softwarelokalisierung_900x755px.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The 'blockquote' tag</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The 'blockquote' tag is used to highlight long quotations within a document. Text enclosed within this tag is traditionally displayed as a block element with left and right margins (usually around 40 pixels) and spacing above and below it. <br>
+									<br>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://static4.businessinsider.com/image/5f372200e89ebf001f044f97-2400/gettyimages-1159828144.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The Project</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The 'big' tag increases the font size by one unit compared to regular text. In HTML, font size is measured in relative units from 1 to 7, with the default medium text size being set to 3. Therefore, adding the 'big' tag increases the text size by one relative unit. You can also use nested 'big' tags, and with each level of nesting, the text size will be larger. <br>
+									<br>
+									 The font size is influenced not only by the size attribute of the 'font' tag but also by the choice of font family. For example, the Arial font appears larger than the Times font, and the Verdana font is slightly larger than Arial. Keep this in mind when selecting fonts and their sizes. For more precise control over text size, it's recommended to use CSS styles.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('https://media.istockphoto.com/photos/development-web-developer-picture-id807196312?k=6&m=807196312&s=612x612&w=0&h=dLtk4Iy59CFCyFvcNU3Oeqs5ll8P5BceSHbtmy4LQl4=') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+</body>
+</html>
